cba140514211a998e5b86bff4f0376d6b821bcc2
Kumaraguru12 <121643762+Kumaraguru12@users.noreply.github.com>
2022-12-30 09:06:30 +0530

    Add files via upload

---
+++ Kumaraguru_DotNetTask_WindowsApp/EmployeeForm.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Kumaraguru_DotNetTask_WindowsApp
+{
+    partial class EmployeeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtEmpCode = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxResigned = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(357, 285);
+            this.txtReport.Margin = new System.Windows.Forms.Padding(4);
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(298, 27);
+            this.txtReport.TabIndex = 0;
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(357, 247);
+            this.txtDept.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(298, 27);
+            this.txtDept.TabIndex = 1;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(357, 324);
+            this.txtContact.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(298, 27);
+            this.txtContact.TabIndex = 2;
+            this.txtContact.TextChanged += new System.EventHandler(this.txtContact_TextChanged);
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(357, 133);
+            this.txtEmpName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(298, 27);
+            this.txtEmpName.TabIndex = 4;
+            // 
+            // txtEmpCode
+            // 
+            this.txtEmpCode.Location = new System.Drawing.Point(357, 95);
+            this.txtEmpCode.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmpCode.Name = "txtEmpCode";
+            this.txtEmpCode.Size = new System.Drawing.Size(298, 27);
+            this.txtEmpCode.TabIndex = 5;
+            this.txtEmpCode.TextChanged += new System.EventHandler(this.txtEmpCode_TextChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(357, 209);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(298, 27);
+            this.dateTimePicker2.TabIndex = 6;
+            this.dateTimePicker2.Value = new System.DateTime(2022, 1, 2, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 99);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Employee Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 181);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 19);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "DOB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 219);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 19);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "DOJ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(220, 251);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 19);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Department";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 289);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 19);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "ReportTo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 330);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Contact";
+            // 
+            // comboBoxResigned
+            // 
+            this.comboBoxResigned.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxResigned.FormattingEnabled = true;
+            this.comboBoxResigned.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.comboBoxResigned.Location = new System.Drawing.Point(357, 362);
+            this.comboBoxResigned.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxResigned.Name = "comboBoxResigned";
+            this.comboBoxResigned.Size = new System.Drawing.Size(142, 27);
+            this.comboBoxResigned.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(220, 364);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 19);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Resigned";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(357, 171);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(298, 27);
+            this.dateTimePicker1.TabIndex = 19;
+            this.dateTimePicker1.Value = new System.DateTime(2000, 4, 1, 0, 0, 0, 0);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(822, 240);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 34);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(822, 311);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(112, 34);
+            this.btnEdit.TabIndex = 21;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(822, 169);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 34);
+            this.btnAdd.TabIndex = 22;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label10
+            // 
+            this.label10.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(13, 38);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(217, 19);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "EMPLOYEE MANAGEMENT";
+            // 
+            // EmployeeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1028, 504);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBoxResigned);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.txtEmpCode);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.txtDept);
+            this.Controls.Add(this.txtReport);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "EmployeeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Management";
+            this.Load += new System.EventHandler(this.EmployeeForm_Load);
+            this.Shown += new System.EventHandler(this.EmployeeForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtEmpCode;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxResigned;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Kumaraguru_DotNetTask_WindowsApp/EmployeeForm.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Kumaraguru_DotNetTask_WindowsApp
+{
+
+    public partial class EmployeeForm : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=.;Initial Catalog = employee_db; Integrated Security = True;");
+
+        public EmployeeForm()
+        {
+            InitializeComponent();
+        }
+        void clearrec()
+        {
+            txtEmpCode.Clear();
+            txtEmpName.Clear();
+            txtReport.Clear();
+            txtDept.Clear();
+            txtContact.Clear();
+            txtEmpCode.Focus();
+        }
+        
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(txtEmpCode.Text=="")
+            {
+                MessageBox.Show("Enter Employee Code", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmpCode.Focus();
+                return;
+            }
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("exec employee_sp '" + int.Parse(txtEmpCode.Text) + "','" + txtEmpName.Text + "','" + DateTime.Parse(dateTimePicker1.Text) + "','" + DateTime.Parse(dateTimePicker2.Text) + "','" + txtDept.Text + "','" + txtReport.Text + "','" + txtContact.Text + "','" + comboBoxResigned.Text + "'", conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data inserted into SQL Database");
+                clearrec();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (txtEmpCode.Text == "")
+            {
+                MessageBox.Show("Enter Employee Code", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmpCode.Focus();
+                return;
+            }
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("exec employee_updatesp '" + int.Parse(txtEmpCode.Text) + "','" + txtEmpName.Text + "','" + DateTime.Parse(dateTimePicker1.Text) + "','" + DateTime.Parse(dateTimePicker2.Text) + "','" + txtDept.Text + "','" + txtReport.Text + "','" + txtContact.Text + "','" + comboBoxResigned.Text + "'", conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Updated to SQL Database");
+                clearrec();
+
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtEmpCode.Text == "")
+            {
+                MessageBox.Show("Enter Employee Code", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmpCode.Focus();
+                return;
+            }
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("exec employee_deletesp '" + int.Parse(txtEmpCode.Text) + "'", conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Deleted From Sql Database");
+                clearrec();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+                     
+        }
+
+       
+
+        private void txtEmpCode_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtEmpCode.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Invalid Entry [ Only Numeric Inputs ]", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmpCode.Text = txtEmpCode.Text.Remove(txtEmpCode.Text.Length - 1);
+            }
+        }
+
+        private void txtContact_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtContact.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Invalid Entry [ Only Numeric Inputs ]", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtContact.Text = txtContact.Text.Remove(txtContact.Text.Length - 1);
+            }
+        }
+
+        private void EmployeeForm_Shown(object sender, EventArgs e)
+        {
+            txtEmpCode.Focus();
+        }
+    }
+}
